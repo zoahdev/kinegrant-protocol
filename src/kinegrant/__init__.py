81c95834f72cfa24d5ff909f470ad71e24ba6d4b
zoahdev <bahuang081@gmail.com>
2026-08-15 07:11:15 +0800

    chore(release): bump reference implementation to 2.14.0 (#113)

--- src/kinegrant/__init__.py
+++ src/kinegrant/__init__.py
@@ -1,6 +1,6 @@
 """KineGrant Protocol reference implementation."""
 
-__version__ = "2.13.0"
+__version__ = "2.14.0"
 
 from .capability import CapabilityIssuer
 from .attenuation import attenuate_capability, verify_attenuation
