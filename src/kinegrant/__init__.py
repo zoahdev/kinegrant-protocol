5f9beac1c5d5cd07fd219c8a57db37b284576b08
zoahdev <bahuang081@gmail.com>
2026-08-15 13:04:57 +0800

    锘緽ump reference implementation to v2.37.0

--- src/kinegrant/__init__.py
+++ src/kinegrant/__init__.py
@@ -1,6 +1,6 @@
 """KineGrant Protocol reference implementation."""
 
-__version__ = "2.36.0"
+__version__ = "2.37.0"
 
 from .capability import CapabilityIssuer
 from .attenuation import attenuate_capability, verify_attenuation
