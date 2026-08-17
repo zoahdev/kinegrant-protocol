12eca30cf2b614b0cb0b5efb41b4658ae7306375
zoahdev <zoahdev@kinegrant.com>
2026-08-17 16:25:07 +0800

    chore(release): bump version to 2.65.5

--- src/kinegrant/__init__.py
+++ src/kinegrant/__init__.py
@@ -1,6 +1,6 @@
 """KineGrant Protocol reference implementation."""
 
-__version__ = "2.65.4"
+__version__ = "2.65.5"
 
 from .capability import CapabilityIssuer
 from .attenuation import attenuate_capability, verify_attenuation
